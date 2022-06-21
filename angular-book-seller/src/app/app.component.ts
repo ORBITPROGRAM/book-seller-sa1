b42003747434291cfffadf0a9bc86281570821df
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-06-21 09:27:36 +0700

    Routing on frontend

--- angular-book-seller/src/app/app.component.ts
+++ angular-book-seller/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  logo = 'assets/images/angular.png'
   title = 'angular-book-seller';
 }
